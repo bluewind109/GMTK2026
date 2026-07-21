ba14d914fc954779815177f1eaf2f8b832d10d46
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-07-22 02:10:17 +0700

    + Add player animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,19 +2,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private float moveSpeed = 5f;
 
     private Vector2 _movement;
     private Rigidbody2D _rb;
+    private Animator _animator;
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+    private int facingDirection = 1;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
+
+        if (_movement.x != 0)
+        {
+            facingDirection = _movement.x > 0 ? 1 : -1;
+            Flip(facingDirection);
+        }
+
+        _animator.SetFloat(HORIZONTAL, Mathf.Abs(_movement.x));
+        _animator.SetFloat(VERTICAL, Mathf.Abs(_movement.y));
+
         _rb.linearVelocity = _movement * moveSpeed;
+
+    }
+
+    private void Flip(int direction)
+    {
+        spriteRenderer.transform.localScale = new Vector3(direction, 1, 1);
     }
 }
